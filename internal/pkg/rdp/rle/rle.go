81b87c5a8bee6c16a6d0262bb85dcc5aeef2692a
lyuvolkova <lvolk14@yandex.com>
2022-06-18 18:22:18 +0300

    new package RLE

---
+++ internal/pkg/rdp/rle/rle.go
@@ -0,0 +1,16 @@
+package rle
+
+//func bitmapDecompress3(output []uint8, width int, height int, input []uint8, size int) bool {
+//	end := input[size - 1]
+//
+//
+//	for i := 0; i < 15; i++ {
+//
+//	}
+//
+//}
+
+func BitmapDecompress24(output []uint8, outputWidth int, outputHeight int, inputWidth int, inputHeight int, input []uint8, size int) int {
+
+	return 0
+}
